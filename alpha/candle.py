5db7a9f1fd6843156290b4b23a7a1dbe13064762
agm-kuraudo <122623005+agm-kuraudo@users.noreply.github.com>
2024-08-01 10:08:30 +0100

    SP-110 - Good progress on working out spread percentiles

--- alpha/candle.py
+++ alpha/candle.py
@@ -35,23 +35,63 @@ class Candle:
             bar_type = "down_bar"
         return "Candle is an {} opened at {} and closed at {}".format(bar_type, self.__open, self.__close)
 
+    @property
+    def spread(self):
+        return self.__spread
 
 class DummyQCTrader:
+    # Set up the short, medium and long term intervals to use.  A bit arbitrary, Eventually can test and tweak
+    PERIOD_ONE_LENGTH = 5
+    PERIOD_TWO_LENGTH = 10
+    PERIOD_THREE_LENGTH = 20
+    PERCENTILE_START = 10
+    PERCENTILE_INCREMENTS = 10
 
     def __init__(self):
-        self.__period1_deque = deque(maxlen=5)
-        self.__period2_deque = deque(maxlen=10)
-        self.__period3_deque = deque(maxlen=20)
+        # We create three "deques" based on those time frames
+        self.deque_dictionary = {
+            "period_one": deque(maxlen=DummyQCTrader.PERIOD_ONE_LENGTH),
+            "period_two": deque(maxlen=DummyQCTrader.PERIOD_TWO_LENGTH),
+            "period_three": deque(maxlen=DummyQCTrader.PERIOD_THREE_LENGTH)
+        }
 
+    # This is a dummy version of what will be the OnData method in QC
     def dummy_on_data(self, time, volume, candle_open, high, low, close):
+        # Make sure we have valid candle data - should always be the case
         if np.isnan(candle_open):
             return
 
+        # Create a new Candle object with the supplied properties
         this_candle = Candle(time, volume, candle_open, high, low, close)
         print(this_candle)
 
-        # TO DO - add candle to deques.  Use queues to work out relative volume/spreads (once they are ready).
-        self.__period1_deque.append(this_candle)
+        ## TODO: All this below should be in a function which returns the percentile figure so we can call if for each
+        # time period without duplicate code
+
+        # If we have Deque period 1 ready lets work out where the volume and spread fall as a percentile
+        # remember we are expecting this on data method to be called frequently - this won't be true on the first
+        # iterations
+        if len(self.deque_dictionary["period_one"]) == DummyQCTrader.PERIOD_ONE_LENGTH:
+            # Get the spread for each candle in the period and save to a list
+            spreads = []
+            for item in self.deque_dictionary["period_one"]:
+                spreads.append(item.spread)
+            # Calculate all the percentiles from 10% upwards in increments
+            current_percentiles = np.percentile(spreads, [range(DummyQCTrader.PERCENTILE_START, 100, DummyQCTrader.PERCENTILE_INCREMENTS)])
+            print(current_percentiles)
+
+            # Loop round and see where the latest figure falls in the percentile list
+            start_check = DummyQCTrader.PERCENTILE_START
+            for data in current_percentiles[0]:
+                if this_candle.spread < data:
+                    print(f"This candle spread is {this_candle.spread} which falls below the {start_check} percentile")
+                    break
+                start_check += DummyQCTrader.PERCENTILE_INCREMENTS
+
+        # Add the new candle to each deque.
+        for key in self.deque_dictionary.keys():
+            self.deque_dictionary[key].append(this_candle)
+
 
 
 # Get our test data from CSV file...
@@ -66,6 +106,11 @@ myDF = myDF.sort_values("Time", axis=0)
 
 print(myDF)
 
+myTrader = DummyQCTrader()
+
+
+
+
 # Loop around each item in the data frame and call my dummy on data method
 for index, row in myDF.iterrows():
-    dummy_on_data(row['Time'], row['Volume'], row['Open'], row['High'], row['Low'], row['Last'])
+    myTrader.dummy_on_data(row['Time'], row['Volume'], row['Open'], row['High'], row['Low'], row['Last'])
