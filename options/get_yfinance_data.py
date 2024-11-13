176d4fff3422b03e04f272b17a06e41035b6e916
agm-kuraudo <122623005+agm-kuraudo@users.noreply.github.com>
2024-11-13 10:55:24 +0000

    SP-113 - Updated download yfinance script to get some data for this ticket

--- options/get_yfinance_data.py
+++ options/get_yfinance_data.py
@@ -1,4 +1,26 @@
 import yfinance as yf
-ticker = "^VIX"
-data = yf.download(ticker, start="2020-11-16", end="2021-11-06")
-data.to_csv("vix_data.csv")
+import matplotlib.pyplot as plt
+# ticker = "^VIX"
+# data = yf.download(ticker, start="2020-11-16", end="2021-11-06")
+# data.to_csv("vix_data.csv")
+
+
+
+data = yf.download('SPY', start='2023-01-02', end='2024-11-12')
+
+data.to_csv("spy_data.csv")
+
+# Plot the close price
+plt.figure(figsize=(15, 7))
+data['Adj Close'].plot()
+
+# Set the title and axis label
+plt.title('SPY Data', fontsize=16)
+plt.xlabel('Year-Month', fontsize=15)
+plt.ylabel('Price', fontsize=15)
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+plt.legend(['Close'], prop={'size': 15})
+
+# Show the plot
+plt.savefig("spy_data.png")
