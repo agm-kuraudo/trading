6f7e4fb5d82940fe10a23f40093880e3ad1c80e9
agm-kuraudo <122623005+agm-kuraudo@users.noreply.github.com>
2025-03-07 15:40:19 +0000

    SP-166 - Moved shared functions to utils folder

--- options/price_calc.py
+++ options/price_calc.py
@@ -1,6 +1,7 @@
 # INPUTS/CONSTANTS
 # GENERAL
-from utils.utils import get_live_data_from_yfinance, return_sample_data, trading_days_between
+from utils.utils import get_live_data_from_yfinance, return_sample_data, trading_days_between, get_asset_data, \
+    process_data, calculate_volatility, calculate_binomial_parameters, price_option
 import numpy as np
 import pandas as pd
 
@@ -9,7 +10,7 @@ PRICING_STEPS = 100
 INTEREST_RATE = 0.04  # This is as percent so 4% is 0.04
 
 # SHARE/OPTION SPECIFIC
-OPTION_EXPIRATION_DATE = '2025-02-21'
+OPTION_EXPIRATION_DATE = '2025-04-17'
 DIVIDEND_YIELD = 0.03
 TICKER = 'AAPL'
 HISTORY_START_DAYS = 380  # Number of days to go back for history - will need to be 365 to get a years worth of data
@@ -20,58 +21,6 @@ option_type = 'call'  # 'call' or 'put'
 OPTION_STYLE = 'AMERICAN'  # or 'EUROPEAN'
 
 use_real_data = True
-
-
-def get_asset_data(use_real_data, ticker, start_days_ago, end_days_ago):
-    if use_real_data:
-        return get_live_data_from_yfinance(ticker=ticker, start_days_ago=start_days_ago, end_days_ago=end_days_ago)
-    else:
-        return return_sample_data()
-
-
-def process_data(data):
-    data = data.sort_values(by=['Date'], ascending=False)
-    data['Diff'] = data['Close'] / data['Close'].shift(1)
-    data.dropna(inplace=True)
-    data['LogDiff'] = data['Diff'].apply(lambda x: np.log(x))
-    return data
-
-
-def calculate_volatility(data, trading_days):
-    data = data.head(trading_days)
-    return data['LogDiff'].std() * np.sqrt(trading_days)
-
-
-def calculate_binomial_parameters(volatility, trading_days_left, trading_days, pricing_steps, interest_rate,
-                                  dividend_yield):
-    delta_t = trading_days_left / (trading_days * pricing_steps)
-    up_branch_move = np.exp(volatility * np.sqrt(delta_t))
-    down_branch_move = 1 / up_branch_move
-    factor_step_discount = np.exp(-interest_rate * delta_t)
-    interest_rate_cost_of_step = np.exp((interest_rate - dividend_yield) * delta_t)
-    up_branch_probability = (interest_rate_cost_of_step - down_branch_move) / (up_branch_move - down_branch_move)
-    down_branch_probability = 1 - up_branch_probability
-    return up_branch_move, down_branch_move, factor_step_discount, up_branch_probability, down_branch_probability
-
-
-def price_option(stock_price_tree, option_value_tree, pricing_steps, up_branch_probability, down_branch_probability,
-                 factor_step_discount, option_type, strike_price, option_style):
-    for i in range(pricing_steps - 1, -1, -1):
-        for j in range(i + 1):
-            option_value = (up_branch_probability * option_value_tree[i + 1, j] +
-                            down_branch_probability * option_value_tree[i + 1, j + 1]) * factor_step_discount
-
-            if option_style == 'AMERICAN':
-                if option_type == 'call':
-                    immediate_exercise_value = max(0, stock_price_tree[i, j] - strike_price)
-                elif option_type == 'put':
-                    immediate_exercise_value = max(0, strike_price - stock_price_tree[i, j])
-
-                option_value_tree[i, j] = max(option_value, immediate_exercise_value)
-            else:
-                option_value_tree[i, j] = option_value
-    return option_value_tree[0, 0]
-
 
 # Main script
 asset_data_frame = get_asset_data(use_real_data, TICKER, HISTORY_START_DAYS, HISTORY_END_DAYS)
--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,9 @@ import os
 import yfinance as yf
 import pandas as pd
 from datetime import datetime, timedelta
+import numpy as np
+from scipy.optimize import newton
+
 
 def get_live_data_from_yfinance(ticker: str = "SPY", start_days_ago: int = 365, end_days_ago: int = 0) -> pd.DataFrame:
 
@@ -52,3 +55,80 @@ def trading_days_between(start_date, end_date):
     # Generate a date range between the start and end dates
     date_range = pd.date_range(start=start_date, end=end_date, freq='B')  # 'B' frequency stands for business days
     return len(date_range)
+
+def process_data(data):
+    data = data.sort_values(by=['Date'], ascending=False)
+    data['Diff'] = data['Close'] / data['Close'].shift(1)
+    data.dropna(inplace=True)
+    data['LogDiff'] = data['Diff'].apply(lambda x: np.log(x))
+    return data
+
+def calculate_volatility(data, trading_days):
+    data = data.head(trading_days)
+    return data['LogDiff'].std() * np.sqrt(trading_days)
+
+def calculate_binomial_parameters(volatility, trading_days_left, trading_days, pricing_steps, interest_rate, dividend_yield):
+    delta_t = trading_days_left / (trading_days * pricing_steps)
+    up_branch_move = np.exp(volatility * np.sqrt(delta_t))
+    down_branch_move = 1 / up_branch_move
+    factor_step_discount = np.exp(-interest_rate * delta_t)
+    interest_rate_cost_of_step = np.exp((interest_rate - dividend_yield) * delta_t)
+    up_branch_probability = (interest_rate_cost_of_step - down_branch_move) / (up_branch_move - down_branch_move)
+    down_branch_probability = 1 - up_branch_probability
+    return up_branch_move, down_branch_move, factor_step_discount, up_branch_probability, down_branch_probability
+
+def price_option(stock_price_tree, option_value_tree, pricing_steps, up_branch_probability, down_branch_probability,
+                 factor_step_discount, option_type, strike_price, option_style):
+    for i in range(pricing_steps - 1, -1, -1):
+        for j in range(i + 1):
+            option_value = (up_branch_probability * option_value_tree[i + 1, j] +
+                            down_branch_probability * option_value_tree[i + 1, j + 1]) * factor_step_discount
+
+            if option_style == 'AMERICAN':
+                if option_type == 'call':
+                    immediate_exercise_value = max(0, stock_price_tree[i, j] - strike_price)
+                elif option_type == 'put':
+                    immediate_exercise_value = max(0, strike_price - stock_price_tree[i, j])
+
+                option_value_tree[i, j] = max(option_value, immediate_exercise_value)
+            else:
+                option_value_tree[i, j] = option_value
+    return option_value_tree[0, 0]
+
+def implied_volatility(option_price, option_type, current_stock_price, strike_price, time_to_expiration, interest_rate,
+                       dividend_yield, pricing_steps, option_style, TRADING_DAYS):
+    def objective_function(volatility):
+        up_branch_move, down_branch_move, factor_step_discount, up_branch_probability, down_branch_probability = calculate_binomial_parameters(
+            volatility, time_to_expiration * TRADING_DAYS, TRADING_DAYS, pricing_steps, interest_rate, dividend_yield)
+
+        stock_price_tree = np.zeros((pricing_steps + 1, pricing_steps + 1))
+        stock_price_tree[0, 0] = current_stock_price
+
+        for i in range(1, pricing_steps + 1):
+            stock_price_tree[i, 0] = stock_price_tree[i - 1, 0] * up_branch_move
+            for j in range(1, i + 1):
+                stock_price_tree[i, j] = stock_price_tree[i - 1, j - 1] * down_branch_move
+
+        option_value_tree = np.zeros((pricing_steps + 1, pricing_steps + 1))
+
+        for j in range(pricing_steps + 1):
+            if option_type == 'call':
+                option_value_tree[pricing_steps, j] = max(0, stock_price_tree[pricing_steps, j] - strike_price)
+            elif option_type == 'put':
+                option_value_tree[pricing_steps, j] = max(0, strike_price - stock_price_tree[pricing_steps, j])
+
+        calculated_option_price = price_option(stock_price_tree, option_value_tree, pricing_steps,
+                                               up_branch_probability,
+                                               down_branch_probability, factor_step_discount, option_type, strike_price,
+                                               option_style)
+        return calculated_option_price - option_price
+
+    initial_guess = 0.2
+    implied_vol = newton(objective_function, initial_guess)
+    return implied_vol
+
+def get_asset_data(use_real_data, ticker, start_days_ago, end_days_ago):
+    if use_real_data:
+        return get_live_data_from_yfinance(ticker=ticker, start_days_ago=start_days_ago, end_days_ago=end_days_ago)
+    else:
+        return return_sample_data()
