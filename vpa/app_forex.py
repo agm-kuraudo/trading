62066a38a04df2711f031e020295749e067ccf14
agm-kuraudo <122623005+agm-kuraudo@users.noreply.github.com>
2025-07-07 09:34:37 +0100

    SP-239 - Checking in selenium work in case I ever want to revert

--- vpa/app_forex.py
+++ vpa/app_forex.py
@@ -1,12 +1,15 @@
+import time
+
 from vpa.app_runner import MarketAnalyzer
 import pandas as pd
 
-my_df = pd.read_csv("data/GBPUSD_D1_CutDown.csv", sep="\t", index_col=False)
 
-print(my_df.head(5))
-my_df.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
-
-print(my_df.head(5))
-
-analyzer = MarketAnalyzer(config_path="config/config.json", log_level="INFO", fixed_df=my_df)
-signal_score = analyzer.process_data()
+# my_df = pd.read_csv("data/GBPUSD_D1_CutDown.csv", sep="\t", index_col=False)
+#
+# print(my_df.head(5))
+# my_df.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
+#
+# print(my_df.head(5))
+#
+# analyzer = MarketAnalyzer(config_path="config/config.json", log_level="INFO", fixed_df=my_df)
+# signal_score = analyzer.process_data()
---
+++ vpa/forex_auto/forex_home_page.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+from vpa.forex_auto.base_page import BasePage
+
+
+class ForexPage(BasePage):
+    __url = "https://www.forexrobotacademy.com/forex-historical-data"
+    __load_button = (By.XPATH, "/html//button[@id='btn-load-data']")
+    __forex_data_download = (By.LINK_TEXT, "GBPUSD_D1.csv")
+    __body_element = (By.XPATH, "//body")
+
+    def __init__(self, driver: WebDriver):
+        super().__init__(driver)
+
+    def open(self):
+        super()._open_url(self.__url)
+        super()._click(self.__body_element)
+
+    def load_data(self):
+        super()._click(self.__load_button)
+
+    def download_data(self):
+        super()._click(self.__forex_data_download)
